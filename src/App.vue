d47c98817e31708b4a821504eda6391d9ac2c4b8
myeong-jae-hwi <audwognl@gmail.com>
2024-10-31 20:00:28 +0900

    초기세팅

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <base-button variant="primary"></base-button>
+  <base-button variant="secondary"></base-button>
+  <base-button color="#ff5733"></base-button> -->
+  <base-calender></base-calender>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseButton from "./components/BaseButton.vue";
+import BaseCalender from "./components/BaseCalender.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BaseButton,
+    BaseCalender,
+  },
+};
 </script>
 
 <style>
+@font-face {
+  font-family: Pretendard;
+  src: url(./assets/font/pretendard/woff2/PretendardVariable.woff2);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  border-block: box-sizing;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Pretendard, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
